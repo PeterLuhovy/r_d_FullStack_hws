c6a0d8c809c96af8b6f0f424eb368f1737a7b452
Peter Luhový <luhovy.peter1@gmail.com>
2025-07-12 02:01:00 +0200

    Add index.js & Update package.json, eslint.config.mj

--- playground/eslint.config.mjs
+++ playground/eslint.config.mjs
@@ -1,22 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import stylistic from "@stylistic/eslint-plugin";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import react from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import stylistic from "@stylistic/eslint-plugin"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
-  { languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  js.configs.recommended,
-  tseslint.configs.recommended,
-  react.configs.flat.recommended,
-  stylistic.configs.customize({
-    indent: 4,
-    quotes: "double",
-    jsx: true,
-    arrowParens: true,
-    quoteProps: "as-needed",
-    commaDangle: "always-multiline"
-  }
-]);
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    react.configs.flat.recommended,
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: "double",
+        jsx: true,
+        arrowParens: true,
+        quoteProps: "as-needed",
+        commaDangle: "always-multiline",
+    }),
+])
